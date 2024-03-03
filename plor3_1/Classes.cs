6cc9662fb2e037e32f65cff52c887433d972f911
Ayumi <shura_matvey@mail.ru>
2024-03-03 20:55:43 +0300

    Full name and age moved to parent class. Added unique person numbers and university-unique record book numbers

--- plor3_1/Classes.cs
+++ plor3_1/Classes.cs
@@ -1,34 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Classes
 {
-    class Student
+    class Student : Person
     {
-        private string name;
-        public string Name
-        {
-            get { return name; }
-            set { name = value ?? "Student hasn't name"; }
-        }
-
-        private string surname;
-        public string Surname
-        {
-            get { return surname; }
-            set { surname = value ?? "Student hasn't surname"; }
-        }
-
-        private int age;
-        public int Age
-        {
-            get { return age; }
-            set
-            {
-                if (value > 0) age = value;
-                else age = 0;
-            }
-        }
-
         private double avg;
         public double Avg
         {
@@ -40,13 +17,15 @@ namespace Classes
             }
         }
 
-        public Student(string name, string surname, int age, double avg)
-        {
-            this.name = name;
-            this.surname = surname;
-            this.age = age;
-            this.avg = avg;
-        }
+        //public student(string name, string surname, int age, double avg)
+        //{
+        //    this.name = name;
+        //    this.surname = surname;
+        //    this.age = age;
+        //    this.avg = avg;
+        //}
+
+        public int record_book;
     }
 
 
@@ -62,6 +41,8 @@ namespace Classes
         public void AddStudent(Student student)
         {
             students.Add(student);
+            record_book_number++;
+            student.record_book = record_book_number;
         }
 
         public void RemoveStudent(Student student)
@@ -69,11 +50,86 @@ namespace Classes
             students.Remove(student);
         }
 
+        public void RemoveStudentByRecordBook(int record_book)
+        {
+            students.Remove(students.FirstOrDefault(student => student.record_book == record_book));
+        }
+
+        public void RemoveStudentByNumber(int number)
+        {
+            students.Remove(students.FirstOrDefault(student => student.Number == number)); ;
+        }
 
         public string FindStudent(Student student)
         {
             if (students.Contains(student)) return "The student has been found";
             else return "The student hasn't been found";
         }
+
+        private int record_book_number = 0;
     }
+
+
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value ?? "Student hasn't name"; }
+        }
+
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value ?? "Student hasn't surname"; }
+        }
+        private string Patronymic { get; set; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0) age = value;
+                else age = 0;
+            }
+        }
+
+        private DateTime BirthDate { get; set; }
+
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+        }
+
+
+        static private int counter = 0;
+
+
+        public Person()
+        {
+            Persons.AddPerson(this);
+            counter++;
+            number = counter;
+        }
+    }
+
+
+    static class Persons 
+    { 
+        private static List<Person> persons = new List<Person>();
+
+        public static void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+    }
+
+    
+
 }
--- plor3_1/Program.cs
+++ plor3_1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Classes;
-using DataAccess;
 using System;
 
 namespace plor3_1
@@ -8,21 +7,26 @@ namespace plor3_1
     {
         static void Main(string[] args)
         {
+            Student student = new Student();
+            Student student2 = new Student();
             University university = new University();
-            Student student1 = new Student("name1", "surname1", 16, 2.8);
-            Student student2 = new Student("name2", "surname2", 17, 2.8);
-            Student student3 = new Student("name3", "surname3", 18, 2.8);
-            university.AddStudent(student1);
-            university.AddStudent(student2);
-            university.AddStudent(student3);
+            University university2 = new University();
 
-            StudentsRepository.WriteToFile(@"Files\Students.txt", university.GetStudents);
+            university.AddStudent(student);
+            university2.AddStudent(student2);
 
-            string[] readed = StudentsRepository.ReadFromFile(@"Files\Students.txt");
-            foreach (string student in readed) 
-            { 
-                Console.WriteLine(student); 
-            }
+            Console.WriteLine(university.GetStudents[0].Number);
+            Console.WriteLine(university2.GetStudents[0].Number);
+            Console.WriteLine(university.GetStudents[0].record_book);
+            Console.WriteLine(university2.GetStudents[0].record_book + "\n");
+
+            Console.WriteLine(university.GetStudents.Count);
+            university.RemoveStudentByNumber(1);
+            Console.WriteLine(university.GetStudents.Count);
+
+            Console.WriteLine(university2.GetStudents.Count);
+            university2.RemoveStudentByRecordBook(1);
+            Console.WriteLine(university2.GetStudents.Count);
         }
     }
 }
